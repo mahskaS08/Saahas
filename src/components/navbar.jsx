140f6f64d1f27671a4f913c789d272e4b062a084
Saksham Wadhawan <wsaksham05@gmail.com>
2025-03-27 00:41:54 +0530

    Mobile Menu d2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+import { motion } from "framer-motion";
 
 
 const Navbar = () => {
@@ -14,7 +15,7 @@ const Navbar = () => {
             <div className='container mx-auto flex justify-between 
             items-center py-4 px-6 md:px-20 lg:px-32
             bg-transparent'>
-            <a href="#Header"><img src="src/assets/logo.svg" alt="logo" className='mr-auto '/></a>
+            <a href="#Header" ><img src="src/assets/logo.svg" alt="logo" className='mr-auto '/></a>
 
             <ul className='hidden md:flex gap-7 text-white text-lg'>
                 <a href="#Header" className='cursor-pointer 
@@ -38,24 +39,34 @@ const Navbar = () => {
             </div>
 
             {isMenuOpen && (
-            <div className='md:hidden bg-white text-black transition-all absolute top-16 left-0 w-full py-4 px-6'>
-                <div className='flex justify-start items-center'> 
-                <h1 className='pb-5 cursor-pointer' onClick={toggleMenu}>X CLOSE</h1>
-                </div>
-                <ul className='flex flex-col gap-4'>
-                <a href="#Header" className='cursor-pointer 
-                hover:text-gray-400 '>ABOUT US</a>
-                <a href="#Projects" className='cursor-pointer
-                hover:text-gray-400 '>PROJECTS</a>
-                <a href="#Pricing" className='cursor-pointer
-                hover:text-gray-400 '>PRICING</a>
-                <a href="#Testimonials" className='cursor-pointer
-                hover:text-gray-400 '>TESTIMONIALS</a>
-                <a href="#Contact" className='cursor-pointer
-                hover:text-gray-400 '>CONTACT US</a>
-                </ul>
-            </div>
-            )}
+    <motion.div 
+        initial={{ x: 100, opacity: 0 }} 
+        animate={{ x: 0, opacity: 1 }} 
+        exit={{ x: 100, opacity: 0 }} 
+        transition={{ type: "spring", stiffness: 120 }}
+        className='md:hidden bg-[rgba(0,0,0,0.78)] backdrop-blur-lg justify-start text-start 
+        text-white transition-all rounded-bl-xl rounded-tl-xl absolute top-20 right-0 w-3/5 max-w-[200px] py-4 px-6 pr-6'>
+        
+        <div className='flex justify-start items-center text-start'> 
+            <h1 className='pb-5 cursor-pointer' onClick={toggleMenu}>X CLOSE</h1>
+        </div>
+        
+        <ul className='flex flex-col gap-4'>
+            {["ABOUT US", "PROJECTS", "PRICING", "TESTIMONIALS", "CONTACT US"].map((item, index) => (
+                <motion.a 
+                    key={index} 
+                    href={`#${item.replace(" ", "")}`} 
+                    className='cursor-pointer hover:text-gray-400'
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.1 * index, duration: 0.4 }}
+                >
+                    {item}
+                </motion.a>
+            ))}
+        </ul>
+    </motion.div>
+)}
         </div>
         );
 };
