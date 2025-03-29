db0765e5d5d9a541237353cea1615b5d1934861a
Saksham Wadhawan <wsaksham05@gmail.com>
2025-03-30 00:32:51 +0530

    added footer

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -74,6 +74,7 @@ const Navbar = () => {
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 0.1 * index, duration: 0.4 }}
+                    onClick={toggleMenu}
                 >
                     {item}
                 </motion.a>
